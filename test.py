ea62758a1fee2e647837fd990cbc683544201bb3
yves848 <yves.godart@gmail.Com>
2022-09-15 15:52:57 +0200

    test

--- test.py
+++ test.py
@@ -6,8 +6,41 @@ class package:
     def __init__(self, line):
         self.name = line[iNom:iID]
         self.id = line[iID: iVersion]
-        self.version = line[iVersion: iDisponible]
-        self.disponible = line[iDisponible: iSource]
+        self.version = line[iVersion: iSource]
+        # self.disponible = line[iDisponible: iSource]
+
+
+def searchPackages(name):
+    l = []
+    scmd = f'winget search {name}'
+    p = subprocess.run(scmd, capture_output=True, shell=True)
+    out = p.stdout.decode('utf-8')
+    sOut = out.split('\r')
+    i = 0
+    while not sOut[i].startswith('Nom'):
+        i += 1
+
+    # Déterminer les positions des champs
+    global iNom
+    iNom = sOut[i].index('Nom')
+    global iID
+    iID = sOut[i].index('ID')
+    global iVersion
+    iVersion = sOut[i].index('Version')
+    # global iDisponible
+    # iDisponible = sOut[i].index('Disponible')
+    global iSource
+    iSource = sOut[i].index('Source')
+
+    i += 2
+
+    while i < len(sOut)-2:
+        s = sOut[i].replace('\n', '')
+        aPackage = package(s)
+        l.append(aPackage)
+        i += 1
+
+    return l
 
 
 def getUpgradables():
@@ -43,6 +76,7 @@ def getUpgradables():
 
 
 if __name__ == "__main__":
-    aList = getUpgradables()
+    # aList = getUpgradables()
+    aList = searchPackages('notepad')
     for item in aList:
         print(item.id)
